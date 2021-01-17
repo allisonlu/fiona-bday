ae8e28612f150e61603f989e2b1c207ba1037770
Allison Lu <allisonlu820@gmail.com>
2021-01-17 00:42:12 -0800

    clean up

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-// get all sketch-src paths and put in array?
+// get all sketch-src paths and put in array
 const all_sketches = document.querySelectorAll('.scene-container');
 
 all_sketches.forEach(sketch => {
-  // let sketch_id = sketch.getAttribute('id');
   let sketch_path = sketch.dataset.sketchSrc;
 
   let iframe = document.createElement('iframe');
