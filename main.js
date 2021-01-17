e872dd128c06261c35f2218144579e4aaf205db8
Allison Lu <allisonlu820@gmail.com>
2021-01-17 00:34:44 -0800

    - dynamically embed p5 canvases onto page

--- main.js
+++ main.js
@@ -1,20 +1,13 @@
 // get all sketch-src paths and put in array?
+const all_sketches = document.querySelectorAll('.scene-container');
 
-let sketch = document.querySelector('#scene-01-drive');
-let sketch_id = sketch.getAttribute('id');
-let sketch_path = sketch.dataset.sketchSrc;
+all_sketches.forEach(sketch => {
+  // let sketch_id = sketch.getAttribute('id');
+  let sketch_path = sketch.dataset.sketchSrc;
 
-console.log(sketch_id)
-console.log(sketch)
-console.log(sketch_path)
-
-// sketch.append(
-//   "<iframe id='sketchframe-"+sketch_id+"' src='"+sketch_path+
-//               "' width='400' height='400' frameborder='0'></iframe>"
-// );
-
-let iframe = document.createElement('iframe');
-iframe.src = sketch_path;
-iframe.height = '400';
-iframe.width = '400';
-sketch.append(iframe);
+  let iframe = document.createElement('iframe');
+  iframe.src = sketch_path;
+  iframe.height = '400';
+  iframe.width = '400';
+  sketch.append(iframe);
+});
